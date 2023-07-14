3dd9bc1925bfb7a7c5fe3ca3471523f765bcfa24
Ivan <skopenkoivan26@gmail.com>
2023-07-14 19:46:40 +0300

    Add files via upload

---
+++ AnimatingThroughJS_Spoiler/js/app.js
@@ -0,0 +1,16 @@
+$('.spoiler').hide();
+
+$('.spoilerButton').on('click', function () {
+  $('.spoilerButton').hide();
+  $('.spoiler').slideDown(2000);
+});
+$('.spoiler').hide();
+
+
+$('.spoiler1').hide();
+
+$('.spoilerButton1').on('click', function () {
+  $('.spoilerButton1').hide();
+  $('.spoiler1').slideDown(2000);
+});
+$('.spoiler1').hide();
